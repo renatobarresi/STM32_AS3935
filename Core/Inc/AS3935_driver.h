a28fa9beedd25a3e26bbe4fbfe79c585fccb82c8
renatobarresi <42518490+renatobarresi@users.noreply.github.com>
2019-12-01 19:57:59 -0300

    Version 1.0

---
+++ Core/Inc/AS3935_driver.h
@@ -0,0 +1,13 @@
+/*
+ * AS3935_driver.h
+ *
+ *  Created on: Dec 1, 2019
+ *      Author: Renato
+ */
+
+#ifndef INC_AS3935_DRIVER_H_
+#define INC_AS3935_DRIVER_H_
+
+
+
+#endif /* INC_AS3935_DRIVER_H_ */
---
+++ Core/Src/AS3935_driver.c
@@ -0,0 +1,8 @@
+/*
+ * AS3935_driver.c
+ *
+ *  Created on: Dec 1, 2019
+ *      Author: Renato
+ */
+
+
